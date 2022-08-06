18112977cf0ce80a8b5f2915530da62af830590f
timothyshen <timothyshen1@hotmail.com>
2022-08-06 10:44:35 +0100

    - Added few missed out files

---
+++ copyright-protection-frontend/src/components/Global/BookSideDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <p>Book Name:</p>
+      <p></p>
+    </div>
+    <div>
+      <button type="button"
+              class="mb-2 w-full inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+        Chapter List
+      </button>
+      <button type="button"
+              class="mb-2 w-full inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+        Edit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookSideDisplay"
+}
+</script>
+
+<style scoped>
+
+</style>
