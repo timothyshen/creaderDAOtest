7010ed576fdb8da7e8254993f7ae6d0a396e60ad
timothyshen <timothyshen1@hotmail.com>
2022-11-06 22:39:44 +0000

    - Added backend

--- copyright-protection-contract/scripts/arguments.js
+++
@@ -1,6 +0,0 @@
-// module.exports = [
-//     "0xE673c45e67Dd7f1266D469F9F2eaEA6DF550C15a", "ipfs://bafkreihdl5mexbqpc7yn5bcjfd7qdzqtoicynujp3gfvbhpvusdua3quue","CreaderDAOAccess", "CRDAT"
-// ]
-module.exports = [
-    'ipfs://bafkreihdl5mexbqpc7yn5bcjfd7qdzqtoicynujp3gfvbhpvusdua3quue', 'CreaderDAO Copyright', 'CRD'
-]
